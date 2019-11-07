bfc5948a3897392d9fc3238a8af72771eb728fa0
Nick Lalic <nick.lalic@gmail.com>
2019-11-07 02:52:25 -0800

    🔥 removing leftover example test file

--- example_test.py
+++
@@ -1,8 +0,0 @@
-from random import random
-from time   import sleep
-
-
-def example_test(axis1, axis2, axis3):
-    print(f'- example_test({axis1}, {axis2}, {axis3}) ', end='')
-    sleep(random())
-    print('✅')
